c307c8f6ecf6db207845a3ccf4f840e2ab70404e
Max Guselnikov <maxgus04@mail.ru>
2024-03-08 19:52:55 +0300

    Добавьте файлы проекта.

---
+++ Pages/Student/MainPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RemoteEduApp.Pages.Student
+{
+    public class MainPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
