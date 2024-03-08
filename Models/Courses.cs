affa91ccaf7959494333b3da4f61b0262d6deb5d
Max Guselnikov <maxgus04@mail.ru>
2024-03-08 21:36:59 +0300

    main page with data

---
+++ Models/Courses.cs
@@ -0,0 +1,9 @@
+﻿namespace RemoteEduApp.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string? SubjectName { get; set; }
+        public string? SubjectShortName { get; set; }
+    }
+}
--- Pages/Student/MainPage.cshtml.cs
+++ Pages/Student/MainPage.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RemoteEduApp.Data;
+using RemoteEduApp.Models;
 
 namespace RemoteEduApp.Pages.Student
 {
+
     public class MainPageModel : PageModel
     {
+
+        public IEnumerable<Courses> CoursesList { get; set; }
+        DataContextDapper _dapper;
+
+        public MainPageModel(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+        }
         public void OnGet()
         {
+            string sql = "select * from courses;";
+            CoursesList = _dapper.LoadData<Courses>(sql);
         }
     }
 }
