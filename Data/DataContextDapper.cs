3169cc7675e4165ce29e04e448bee5759c24845a
Max Guselnikov <maxgus04@mail.ru>
2024-03-08 20:39:00 +0300

    Dapper added

--- Data/DataContextDapper.cs
+++ Data/DataContextDapper.cs
@@ -6,37 +6,34 @@ namespace RemoteEduApp.Data
 {
     public class DataContextDapper
     {
-        class DataContextDapper
+        private readonly IConfiguration _config;
+        public DataContextDapper(IConfiguration config)
         {
-            private readonly IConfiguration _config;
-            public DataContextDapper(IConfiguration config)
-            {
-                _config = config;
-            }
+            _config = config;
+        }
 
-            public IEnumerable<T> LoadData<T>(string sql)
-            {
-                IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-                return dbConnection.Query<T>(sql);
-            }
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Query<T>(sql);
+        }
 
-            public T LoadDataSingle<T>(string sql)
-            {
-                IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-                return dbConnection.QuerySingle<T>(sql);
-            }
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.QuerySingle<T>(sql);
+        }
 
-            public bool ExecuteSql(string sql)
-            {
-                IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-                return dbConnection.Execute(sql) > 0;
-            }
+        public bool ExecuteSql(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Execute(sql) > 0;
+        }
 
-            public int ExecuteSqlWithRows(string sql)
-            {
-                IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-                return dbConnection.Execute(sql);
-            }
+        public int ExecuteSqlWithRows(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Execute(sql);
         }
     }
 }
