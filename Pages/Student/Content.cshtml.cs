2ff9403074269bee962c62efaa56a1869f9a10ef
Max Guselnikov <maxgus04@mail.ru>
2024-03-13 13:04:37 +0300

    Adding content

--- Pages/Student/Content.cshtml.cs
+++ Pages/Student/Content.cshtml.cs
@@ -3,10 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RemoteEduApp.Pages.Student
 {
-    public class LessonModel : PageModel
+    public class ContentModel : PageModel
     {
         public void OnGet()
         {
+
         }
     }
 }
