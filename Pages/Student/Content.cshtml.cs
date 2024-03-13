876803a70ffdf18fb57e758df7382c648cbf14f6
Max Guselnikov <maxgus04@mail.ru>
2024-03-13 21:07:16 +0300

    Content pages added

--- Pages/Student/Content.cshtml.cs
+++ Pages/Student/Content.cshtml.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RemoteEduApp.Data;
+using RemoteEduApp.Models;
 
 namespace RemoteEduApp.Pages.Student
 {
     public class ContentModel : PageModel
     {
+        string _errorMessage = "";
+        public Content PageContent { get; set; }
+        DataContextDapper _dapper;
+        public string ErrorMessage { get => _errorMessage; set => _errorMessage = value; }
+
+        public ContentModel(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+        }
         public void OnGet()
         {
+            string? ContentId = Request.Query["id"];
+            string sql = "SELECT * FROM RemoteEduDB.dbo.Сontent WHERE Id = " + ContentId;
+            PageContent = _dapper.LoadDataSingle<Content>(sql);
 
+            if (PageContent == null)
+            {
+                ErrorMessage = "Здесь еще нет материала!";
+            }
         }
     }
 }
